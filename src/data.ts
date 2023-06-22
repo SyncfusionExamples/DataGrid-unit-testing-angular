544b61a39dff2fdbb12a5092a09474b06e05f34a
jmfarook7 <93311100+jmfarook7@users.noreply.github.com>
2023-06-22 17:15:18 +0530

    Add files via upload

---
+++ src/data.ts
@@ -0,0 +1,99 @@
+export let stringData = [
+  {
+    "OrderID": 10248,
+    "CustomerID": "VINET",
+    "OrderDate": "1996-07-04T00:00:00.000Z",
+    "ShippedDate": "1996-07-16T00:00:00.000Z",
+    "Freight": 32.38,
+    "ShipName": "Vins et alcools Chevalier",
+    "ShipAddress": "59 rue de l'Abbaye",
+    "ShipCity": "Reims",
+    "ShipRegion": null,
+    "ShipCountry": "France"
+  },
+  {
+    "OrderID": 10249,
+    "CustomerID": "TOMSP",
+    "OrderDate": "1996-07-05T00:00:00.000Z",
+    "ShippedDate": "1996-07-10T00:00:00.000Z",
+    "Freight": 11.61,
+    "ShipName": "Toms Spezialitäten",
+    "ShipAddress": "Luisenstr. 48",
+    "ShipCity": "Münster",
+    "ShipRegion": null,
+    "ShipCountry": "Germany"
+  },
+  {
+    "OrderID": 10250,
+    "CustomerID": "HANAR",
+    "OrderDate": "1996-07-08T00:00:00.000Z",
+    "ShippedDate": "1996-07-12T00:00:00.000Z",
+    "Freight": 65.83,
+    "ShipName": "Hanari Carnes",
+    "ShipAddress": "Rua do Paço, 67",
+    "ShipCity": "Rio de Janeiro",
+    "ShipRegion": "RJ",
+    "ShipCountry": "Brazil"
+  },
+  {
+    "OrderID": 10251,
+    "CustomerID": "VICTE",
+    "OrderDate": "1996-07-08T00:00:00.000Z",
+    "ShippedDate": "1996-07-15T00:00:00.000Z",
+    "Freight": 41.34,
+    "ShipName": "Victuailles en stock",
+    "ShipAddress": "2, rue du Commerce",
+    "ShipCity": "Lyon",
+    "ShipRegion": null,
+    "ShipCountry": "France"
+  },
+  {
+    "OrderID": 10248,
+    "CustomerID": "VINET",
+    "OrderDate": "1996-07-04T00:00:00.000Z",
+    "ShippedDate": "1996-07-16T00:00:00.000Z",
+    "Freight": 32.38,
+    "ShipName": "Vins et alcools Chevalier",
+    "ShipAddress": "59 rue de l'Abbaye",
+    "ShipCity": "Reims",
+    "ShipRegion": null,
+    "ShipCountry": "France"
+  },
+  {
+    "OrderID": 10249,
+    "CustomerID": "TOMSP",
+    "OrderDate": "1996-07-05T00:00:00.000Z",
+    "ShippedDate": "1996-07-10T00:00:00.000Z",
+    "Freight": 11.61,
+    "ShipName": "Toms Spezialitäten",
+    "ShipAddress": "Luisenstr. 48",
+    "ShipCity": "Münster",
+    "ShipRegion": null,
+    "ShipCountry": "Germany"
+  },
+  {
+    "OrderID": 10250,
+    "CustomerID": "HANAR",
+    "OrderDate": "1996-07-08T00:00:00.000Z",
+    "ShippedDate": "1996-07-12T00:00:00.000Z",
+    "Freight": 65.83,
+    "ShipName": "Hanari Carnes",
+    "ShipAddress": "Rua do Paço, 67",
+    "ShipCity": "Rio de Janeiro",
+    "ShipRegion": "RJ",
+    "ShipCountry": "Brazil"
+  },
+  {
+    "OrderID": 10251,
+    "CustomerID": "VICTE",
+    "OrderDate": "1996-07-08T00:00:00.000Z",
+    "ShippedDate": "1996-07-15T00:00:00.000Z",
+    "Freight": 41.34,
+    "ShipName": "Victuailles en stock",
+    "ShipAddress": "2, rue du Commerce",
+    "ShipCity": "Lyon",
+    "ShipRegion": null,
+    "ShipCountry": "France"
+  }
+
+];
