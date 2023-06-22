e0761aa13e6338b888a6d9c1a316f34cbfbeb869
jmfarook7 <93311100+jmfarook7@users.noreply.github.com>
2023-06-22 17:16:54 +0530

    Add files via upload

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { GridComponent, GridAllModule } from '@syncfusion/ej2-angular-grids';
+
+describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+      imports: [
+        GridAllModule
+      ],
+      declarations: [
+        AppComponent,
+        GridComponent
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it(`Length of the record`, fakeAsync(async () => {
+    debugger;
+    fixture.detectChanges();
+    tick(1000);
+    expect(component.grid.currentViewData.length).toBe(8);
+  }));
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
+
+import { stringData } from '../data';
+
+@Component({
+    selector: 'app-root',
+    template: `<ejs-grid #Grid [dataSource]='data' [allowSorting]='true'>
+                <e-columns>
+                    <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=120></e-column>
+                    <e-column field='CustomerID' headerText='Customer ID' width=150></e-column>
+                    <e-column field='ShipCity' headerText='Ship City' width=150></e-column>
+                    <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
+                </e-columns>
+                </ejs-grid>`
+})
+export class AppComponent implements OnInit {
+  @ViewChild('Grid')
+  public grid: GridComponent;
+    public data: object[];
+    ngOnInit(): void {
+        this.data = stringData;
+    }
+}
