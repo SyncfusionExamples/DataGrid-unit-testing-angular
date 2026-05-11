832ac3ca0d271af87f47f8fabcab36fb28190112
Sivaranjani Rajasekaran <sivaranjani.r@syncfusion.com>
2026-05-11 19:21:15 +0530

    Documentation(1023562): Update package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {  GridAllModule } from '@syncfusion/ej2-angular-grids';
-import { CommonModule, DatePipe } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GridAllModule,
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
